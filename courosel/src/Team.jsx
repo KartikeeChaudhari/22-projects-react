982e215285f53f887ee883c5fa0a0db2018b92af
kc <sc571861@company.com>
2024-07-06 15:58:14 +0530

    Added teams and css file

---
+++ courosel/src/Team.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import data from './Data'
+
+const Team = () => {
+  return (
+    <>
+    <div className='container'>
+        <h1>Hi</h1>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default Team
