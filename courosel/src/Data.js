809a939b45aad041d271a812f8f65b05f04cafa8
kc <sc571861@company.com>
2024-07-06 15:53:09 +0530

    Data file added

---
+++ courosel/src/Data.js
@@ -0,0 +1,27 @@
+const data=[{
+    id:1,
+    name:"Chirag Paswan",
+    job:"Politiian",
+    image:"https://assets.telegraphindia.com/telegraph/2021/Jun/1624194867_untitled-design-2021-06-20t184348-448.jpg"
+},
+{
+    id:2,
+    name:"Virat Kohli",
+    job:"Cricketer",
+    image:"https://th.bing.com/th/id/OIP.EEV_ZTRvdGP5KW3j3wEchgHaNK?rs=1&pid=ImgDetMain"
+},
+{
+    id:3,
+    name:"Samantha Ruth Prabhu",
+    job:"Actress",
+    image:"https://th.bing.com/th/id/OIP.orHBJjAUvpaKkf0L2JwKEAHaJK?rs=1&pid=ImgDetMain"
+},
+{
+    id:4,
+    name:"Anushka Sharma",
+    job:"Actress",
+    image:"https://i.redd.it/y1ptcjka3ly41.jpg"
+}
+]
+
+export default data;
